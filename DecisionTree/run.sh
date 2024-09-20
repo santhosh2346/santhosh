0d130fada3d7c54880535bf22edc6f7808d6fb52
Bindu Sri Sai Uppalapati <uppalapatisrisaisanthosh@gmail.com>
2024-09-20 18:35:24 -0400

    Added run.sh

---
+++ DecisionTree/run.sh
@@ -0,0 +1,7 @@
+echo "Results for Decision Tree Classifier"
+
+jupyter notebook HHw-1-Q2-DecisionTree-Practice-Car.ipynb
+
+echo "Results for Decision Tree Classifier"
+
+jupyter notebook HHw-1-Q3-Bank-marketing.ipynb
