9a83da0d509b7408c46d0a1a21e37946cc8fb250
Bindu Sri Sai Uppalapati <uppalapatisrisaisanthosh@gmail.com>
2024-10-17 20:56:00 -0400

    Added all files for HW2

--- DecisionTree/run.sh
+++ DecisionTree/run.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
 
-echo "Results for Decision Tree Classifier"
+echo "Results for AdaBoost Algorithm"
+python3 HW_2_2a.ipynb
 
-jupyter notebook Hw-1-Q2-DecisionTree-Practice-Car.ipynb
+echo "Results for Bagged Trees Algorithm"
+python3 HW_2_2b_2c.ipynb
 
-echo "Results for Decision Tree Classifier"
+echo "Results for bias and variance decomposition"
+python3 HW_2_2b_2c.ipynb
 
-jupyter notebook Hw-1-Q3-Bank-marketing.ipynb
+echo "Results for random forest algorithm"
+python3 2d.ipynb
+
+echo "Results for bias and variance, squared error decomposition"
+python3 HW_2_2b.ipynb
+
+#!/bin/sh
+
+echo "Results for batch gradient descent algorithm"
+python3 HW_2_4a.ipynb
+
+echo "Results for stochastic gradient descent (SGD) algorithm"
+python3 HW_2_4b_4c.ipynb
+
+echo "Results for  calculate the optimal weight vector"
+python3 HW_2_4b_4c.ipynb
+
+
+
+
+
