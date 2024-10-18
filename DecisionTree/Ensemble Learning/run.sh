1c14106299c709a8aa21d3a8f85108d7c9888997
Bindu Sri Sai Uppalapati <uppalapatisrisaisanthosh@gmail.com>
2024-10-17 20:54:34 -0400

    Added all files for HW2

---
+++ DecisionTree/Ensemble Learning/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Results for AdaBoost Algorithm"
+python3 HW_2_2a.ipynb
+
+echo "Results for Bagged Trees Algorithm"
+python3 HW_2_2b_2c.ipynb
+
+echo "Results for bias and variance decomposition"
+python3 HW_2_2b_2c.ipynb
+
+echo "Results for random forest algorithm"
+python3 2d.ipynb
+
+echo "Results for bias and variance, squared error decomposition"
+python3 HW_2_2b.ipynb
---
+++ DecisionTree/Linear Regression/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Results for batch gradient descent algorithm"
+python3 HW_2_4a.ipynb
+
+echo "Results for stochastic gradient descent (SGD) algorithm"
+python3 HW_2_4b_4c.ipynb
+
+echo "Results for  calculate the optimal weight vector"
+python3 HW_2_4b_4c.ipynb
